7a88cc2c2f24981d599daada68959149b31ae35c
sonu7524 <sonudaryani135@gmail.com>
2022-12-16 14:52:48 +0530

    User Login & register portal

---
+++ src/main/java/LoginController.java
@@ -0,0 +1,33 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+
+public class LoginController {
+
+    @FXML
+    private Hyperlink fogotpass;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button register;
+
+    @FXML
+    void forgotpasslink(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerButton(ActionEvent event) {
+
+    }
+
+}
+
